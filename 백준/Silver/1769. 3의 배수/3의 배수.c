94ade92d656b465744c14df810308595b9db0af4
IamDongmin <jkjk6022@naver.com>
2022-09-18 20:38:22 +0900

    Update 3의 배수.c

--- 백준/Silver/1769. 3의 배수/3의 배수.c
+++ 백준/Silver/1769. 3의 배수/3의 배수.c
@@ -1,71 +1,53 @@
 #include <stdio.h>
-#include <string.h>
 
-void sumAll(char*, int*);
-void pasteInt(char*, int);
-int isThree(int);
+int cnt = 0;
 
-int main() {
+int multipleof3(int X)
+{
+	int sum = 0, count = 0, num;
 
-	char n[1000001];
-	int int_n = 0, count = 0;
+	num = X;
 
-	scanf("%s", n);
-
-	if (strlen(n) == 1) 
+	while (1)
 	{
-		sumAll(n, &int_n);
+		sum += (num % 10);
+		num /= 10;
+
+		count++;
+
+		if (num == 0)
+		{
+			break;
+		}
+	}
+
+	if (count == 1)
+	{
+		return X;
+	}
+
+	cnt++;
+
+	multipleof3(sum);
+}
+int main()
+{
+	int X, result;
+
+	scanf_s("%d", &X);
+
+	result = multipleof3(X);
+
+	printf("%d\n", cnt);
+
+	if (result == 3 || result == 6 || result == 9)
+	{
+		printf("YES\n");
 	}
 	else 
 	{
-		int_n = 10;
-	}
-
-	while (int_n >= 10) 
-	{
-		sumAll(n, &int_n);
-		pasteInt(n, int_n);
-		count++;
-	}
-
-	printf("%d\n", count);
-
-	if (isThree(int_n))
-	{
-		printf("YES");
-	}	
-	else
-	{
-		printf("NO");
+		printf("N0\n");
 	}
 
 	return 0;
-}
-
-void sumAll(char* str, int* n) 
-{
-	int i;
-
-	*n = 0;
-
-	for (i = 0; str[i]; i++) 
-	{
-		(*n) += (str[i] - '0');
-	}
-}
-
-void pasteInt(char* str, int n) 
-{
-	int i = 0;
-
-	while (n > 0) 
-	{
-		str[i++] = (n % 10) + '0';
-		n /= 10;
-	}
-	str[i] = NULL;
-}
-int isThree(int n) 
-{
-	return n == 3 || n == 6 || n == 9;
 }
