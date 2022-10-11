4d776af86e46dc3b5f276c852a21ccd210f78c71
IamDongmin <jkjk6022@naver.com>
2022-10-11 23:34:08 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long a, b, c;
+    
+	cin >> a >> b >> c;
+    
+	if (a + b - 2 * c >= 0) 
+        cout << a + b - 2 * c << endl;
+	else 
+        cout << a + b << endl;
+    
+    return 0;
+}
