a2248dc79748c4a4755752cf36060fc704f55343
IamDongmin <jkjk6022@naver.com>
2022-11-20 23:57:02 +0900

    [Bronze IV] Title: 헛간 청약, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19698. 헛간 청약/헛간 청약.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n, w, h, l;
+    
+    cin >> n >> w >> h >> l;
+    
+    int ans = (w / l) * (h / l);
+    
+    n < ans ? cout << n : cout << ans;
+    
+    return 0;
+}
